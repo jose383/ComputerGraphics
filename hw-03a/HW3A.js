8e1a0b5147059cb1e62dc71e9c964f1782b9d94f
Jose Lopez <jose383@gmail.com>
2019-09-19 21:15:50 -0700

    added colors, everything works fine!

--- hw-03a/HW3A.js
+++ hw-03a/HW3A.js
@@ -7,6 +7,14 @@
  **/
 
 let gl;
+const original = [
+    -0.3, -0.2,
+    -0.3,  0.2,
+    0.0, 0.4,
+    0.3, 0.2,
+    0.3, -0.2,
+    0.0, -0.4
+];
 
 window.onload = function init()
 {
@@ -15,14 +23,16 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+    // Vertices
 
-    // Four Vertices
+    let vertices = original.slice(0);
 
-    let vertices = [
-        -0.3, -0.3,
-        -0.3,  0.3,
-        0.3, 0.3,
-        0.3, -0.3
+    let colors = [
+        vec4(0.4,0.0,0.3,1.0),
+        vec4(0.2,0.1,0.7,1.0),
+        vec4(0.3,0.6,0.6,1.0),
+        vec4(0.2,0.5,0.3,1.0),
+        vec4(0.2,0.2,0.6,1.0)
     ];
 
     //
@@ -36,11 +46,32 @@ window.onload = function init()
     let program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    // Load Colors
+
+    let cBufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    let vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
     // Load the data into the GPU
 
     let bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+
+    // Associate out shader variables with our data buffer
+
+    let vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    //gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+    render();
 
     // Keyboard Listener
 
@@ -50,95 +81,71 @@ window.onload = function init()
 
         switch (e.key) {
             case 'a':
+
                 // Left
+
                 if (!(vertices[0] <= -1.0 && vertices[2] <= -1.0)) {
-                    vertices[0] -= move; // Lower Left RIGHT/LEFT -
-                    //vertices[1] += move; // Lower Left UP/DOWN -
-                    vertices[2] -= move; // Upper Left RIGHT/LEFT -
-                    //vertices[3] += move; // Upper Left UP/DOWN +
-                    vertices[4] -= move; // Upper Right RIGHT/LEFT +
-                    //vertices[5] += move; // Upper Right UP/DOWN +
-                    vertices[6] -= move; // Lower Right RIGHT/LEFT +
-                    //vertices[7] += move; // Lower Right UP/DOWN -
+                    for (let i = 0; i < vertices.length; i+=2)
+                        vertices[i] -= move;
                 }
 
                 gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
                 render();
                 return;
+
             case 'd':
+
                 // Right
-                if (!(vertices[4] >= 1.0 && vertices[6] >= 1.0)){
-                    vertices[0] += move; // Lower Left RIGHT/LEFT -
-                    //vertices[1] += move; // Lower Left UP/DOWN -
-                    vertices[2] += move; // Upper Left RIGHT/LEFT -
-                    //vertices[3] += move; // Upper Left UP/DOWN +
-                    vertices[4] += move; // Upper Right RIGHT/LEFT +
-                    //vertices[5] += move; // Upper Right UP/DOWN +
-                    vertices[6] += move; // Lower Right RIGHT/LEFT +
-                    //vertices[7] += move; // Lower Right UP/DOWN -
+
+                if (!(vertices[6] >= 1.0 && vertices[8] >= 1.0)){
+                    for (let i = 0; i < vertices.length; i+=2)
+                        vertices[i] += move;
                 }
 
                 gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
                 render();
                 return;
+
             case 'w':
+
                 // Up
-                if (!(vertices[3] >= 1.0 && vertices[5] >= 1.0)){
-                    //vertices[0] += move; // Lower Left RIGHT/LEFT -
-                    vertices[1] += move; // Lower Left UP/DOWN -
-                    //vertices[2] += move; // Upper Left RIGHT/LEFT -
-                    vertices[3] += move; // Upper Left UP/DOWN +
-                    //vertices[4] += move; // Upper Right RIGHT/LEFT +
-                    vertices[5] += move; // Upper Right UP/DOWN +
-                    //vertices[6] += move; // Lower Right RIGHT/LEFT +
-                    vertices[7] += move; // Lower Right UP/DOWN -
+
+                if (!(vertices[5] >= 1.0)){
+                    for (let i = 1; i < vertices.length; i+=2)
+                        vertices[i] += move;
                 }
 
                 gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
                 render();
                 return;
+
             case 's':
+
                 // Down
-                if (!(vertices[1] <= -1.0 && vertices[7] <= -1.0)){
-                    //vertices[0] += move; // Lower Left RIGHT/LEFT -
-                    vertices[1] -= move; // Lower Left UP/DOWN -
-                    //vertices[2] += move; // Upper Left RIGHT/LEFT -
-                    vertices[3] -= move; // Upper Left UP/DOWN +
-                    //vertices[4] += move; // Upper Right RIGHT/LEFT +
-                    vertices[5] -= move; // Upper Right UP/DOWN +
-                    //vertices[6] += move; // Lower Right RIGHT/LEFT +
-                    vertices[7] -= move; // Lower Right UP/DOWN -
+
+                if (!(vertices[11] <= -1.0)){
+                    for (let i = 1; i < vertices.length; i+=2)
+                        vertices[i] -= move;
                 }
 
                 gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
                 render();
                 return;
+
             case '1':
-                vertices = [
-                    -0.3, -0.3,
-                    -0.3,  0.3,
-                    0.3, 0.3,
-                    0.3, -0.3
-                ];
+
+                // Back to Original
+
+                vertices = original.slice(0);
                 gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
                 render();
                 return;
         }
-
-
-
     });
-    // Associate out shader variables with our data buffer
-
-    let vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    render();
 };
 
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, original.length/2 );
 }

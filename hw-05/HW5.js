177aa8fa1d244ef7a3d74dd051b134622832862f
Jose Lopez <jose383@gmail.com>
2019-10-16 00:35:34 -0700

    Smoother rotation and changed 'var' for 'let'

--- hw-05/HW5.js
+++ hw-05/HW5.js
@@ -1,18 +1,18 @@
 "use strict";
 
-var canvas;
-var gl;
+let canvas;
+let gl;
 
-var numVertices  = 12;//36;
+let numVertices  = 12;//36;
 
-var pointsArray = [];
-var normalsArray = [];
+let pointsArray = [];
+let normalsArray = [];
 
-var vertices = [
+let vertices = [
     vec4( -0.5, -0.5,  0.5, 1.0 ),
     //vec4( -0.5,  0.5,  0.5, 1.0 ),
     //vec4( 0.5,  0.5,  0.5, 1.0 ),
-		vec4(0, 0.5, 0),
+    vec4(0, 0.5, 0),
     vec4( 0.5, -0.5,  0.5, 1.0 ),
     //vec4( -0.5, -0.5, -0.5, 1.0 ),
     //vec4( -0.5,  0.5, -0.5, 1.0 ),
@@ -20,40 +20,40 @@ var vertices = [
     vec4( 0.5, -0.5, -0.5, 1.0 )
 ];
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
-var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
-var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+let lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+let lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+let lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+let lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
-var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
-var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
-var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
-var materialShininess = 100.0;
+let materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+let materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
+let materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+let materialShininess = 100.0;
 
-var ctm;
-var ambientColor, diffuseColor, specularColor;
-var modelView, projection;
-var viewerPos;
-var program;
+let ctm;
+let ambientColor, diffuseColor, specularColor;
+let modelView, projection;
+let viewerPos;
+let program;
 
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
-var axis = 0;
-var theta =[0, 0, 0];
+let xAxis = 0;
+let yAxis = 1;
+let zAxis = 2;
+let axis = 0;
+let theta =[0, 0, 0];
 
-var thetaLoc;
+let thetaLoc;
 
-var flag = false;
+let flag = false;
 
-var rotateVal = 2.0;
+let rotateVal = 2.0;
 
 function quad(a, b, c, d) {
 
-    var t1 = subtract(vertices[b], vertices[a]);
-    var t2 = subtract(vertices[c], vertices[b]);
-    var normal = cross(t1, t2);
-    var normal = vec3(normal);
+    let t1 = subtract(vertices[b], vertices[a]);
+    let t2 = subtract(vertices[c], vertices[b]);
+    let normal = cross(t1, t2);
+    normal = vec3(normal);
 
 
     pointsArray.push(vertices[a]);
@@ -73,10 +73,10 @@ function quad(a, b, c, d) {
 
 function colorCube()
 {
-	quad(0, 2, 1, 0);
-	quad(2, 3, 1, 0);
-	quad(1, 3, 0, 0);
-	quad(3, 2, 0, 0);
+    quad(0, 2, 1, 0);
+    quad(2, 3, 1, 0);
+    quad(1, 3, 0, 0);
+    quad(3, 2, 0, 0);
     /*quad( 1, 0, 3, 2 );
     quad( 2, 3, 7, 6 );
     quad( 3, 0, 4, 7 );
@@ -105,19 +105,19 @@ window.onload = function init() {
 
     colorCube();
 
-    var nBuffer = gl.createBuffer();
+    let nBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
 
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    let vNormal = gl.getAttribLocation( program, "vNormal" );
     gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vNormal );
 
-    var vBuffer = gl.createBuffer();
+    let vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    let vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
@@ -127,14 +127,9 @@ window.onload = function init() {
 
     projection = ortho(-1, 1, -1, 1, -100, 100);
 
-    var ambientProduct = mult(lightAmbient, materialAmbient);
-    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    var specularProduct = mult(lightSpecular, materialSpecular);
-
-    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
-    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
-    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+    let ambientProduct = mult(lightAmbient, materialAmbient);
+    let diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    let specularProduct = mult(lightSpecular, materialSpecular);
 
     gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
         flatten(ambientProduct));
@@ -157,53 +152,43 @@ window.onload = function init() {
 
         switch (e.key) {
             case 'ArrowUp':
-                //rotateVal = -2.0;
                 rotateVal = (rotateVal > 0) ? rotateVal * -1.0 : rotateVal;
 
                 axis = xAxis;
-                theta[axis] += rotateVal;
+                flag = true;
 
-                console.log("Up");
                 break;
 
             case 'ArrowDown':
-                //rotateVal = 2.0;
                 rotateVal = (rotateVal < 0) ? rotateVal * -1.0 : rotateVal;
 
                 axis = xAxis;
-                theta[axis] += rotateVal;
+                flag = true;
 
-                console.log("Down");
                 break;
 
             case 'ArrowLeft':
-                //rotateVal = -2.0;
                 rotateVal = (rotateVal > 0) ? rotateVal * -1.0 : rotateVal;
 
                 axis = yAxis;
-                theta[axis] += rotateVal;
-                console.log("Left");
+                flag = true;
+
                 break;
 
             case 'ArrowRight':
-                //rotateVal = 2.0;
                 rotateVal = (rotateVal < 0) ? rotateVal * -1.0 : rotateVal;
 
                 axis = yAxis;
-                theta[axis] += rotateVal;
+                flag = true;
 
-                console.log("Right");
-                break;
-
-            case 't':
-                flag = !flag;
-                console.log("Toggle Rotation");
                 break;
         }
     });
+
+    document.addEventListener('keyup', () => { flag = false; });
 };
 
-var render = function(){
+let render = function(){
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
